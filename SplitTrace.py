ee17f01cfa3ec255166490672fe288f4d9bd783f
Georgy Firsov <gfirsov007@gmail.com>
2019-09-24 21:59:34 +0300

    Cosmetic

--- SplitTrace.py
+++ SplitTrace.py
@@ -1,7 +1,8 @@
 import sys
 import re
 from time import time
-time0 = time()
+
+
 def make_breaking_up(line : str, temp_f):
 	reg = r'^\d{2}\.\d{2}\.\d{4}\ \d{2}\:\d{2}\:\d{2}'
 	result = re.match(reg, line)
@@ -14,14 +15,17 @@ def make_breaking_up(line : str, temp_f):
 			temp_f = open( 'trace_' + br_up_item + '.log', 'w')
 			opened_f[br_up_item] = temp_f
 			temp_f.write(line)
-
 	else:
 		temp_f.write(line)
 	return temp_f
 
+
 def clear_opened_f(opened_f):
 	for item in opened_f.keys():
 		opened_f[item].close()
+		
+
+time0 = time()
 
 if __name__ == "__main__":
 	first = sys.argv[1] if len(sys.argv) > 1 else exit('bad args')
@@ -34,11 +38,10 @@ if __name__ == "__main__":
 	temp_f = open('_Unknown_.log', 'w')
 	opened_f = {'Unknown': temp_f}
 
-
-
 	for line in f:
 		temp_f =  make_breaking_up(line, temp_f)
 
 	clear_opened_f(opened_f)
 	f.close()
+	
 print(time() - time0)
